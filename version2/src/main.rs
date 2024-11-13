9cd1be2b77b22a8a267805603ec8a5f812983dd4
lozlof <lozlofcyber@outlook.com>
2024-11-13 15:48:41 +0000

    Save

--- version2/src/main.rs
+++ version2/src/main.rs
@@ -12,33 +12,22 @@ struct Options {
 }
 
 impl Options {
-    fn interpret_and_build_syntax(collected_options: Vec<String>) {
-
+    fn interpret_and_build_syntax(collected_arguments: Vec<String>) {
+        let all_possible_options: [&str; 14] = ["--help", "-h", "--version", "-ver", "--verbose", "-v", "--query", "-q", "--path", "-p", "--simple-grep", "-sg", "--simple-find", "-sf"];
     }
 }
 
 fn main() {
-    let collected_options_main: Vec<String> = env::args().skip(1).collect(); // Will collect passed arguments and put them into a vector. All arguments are options. Does not collect the first passed argument, because it is not needed.
+    let collected_arguments_main: Vec<String> = env::args().skip(1).collect(); // Will collect passed arguments and put them into a vector. Does not collect the first passed argument, because it is not needed.
 
-    let running_configuration= Options::interpret_and_build_syntax(collected_options_main);
+    initial_verify_options(&collected_arguments_main); // Initial_verify_options checks if zero arguments were passed, and if too many arguments were passed, in either senario the process will exit.. 
 
+    let running_configuration= Options::interpret_and_build_syntax(collected_arguments_main);
 
-
-    // let possible_options: [&'static str; 12] = ["--help", "-h", "--path", "-p", "--simple-grep", "-sg", "--simple-find", "-sf", "--verbose", "-v", "--version", "-ver", "--query", "-q"]; // Array of all the possible options.
     // let total_num_collected_args: usize = collected_options.len(); // Gets the length of the collected options.
 
     // println!("{}", collected_options.len());
     // println!("{}", collected_options[0]);
-
-    if collected_options.len() < 1 { // If one or less options are passed, there is an issue.
-        println!("Not enough options were passed");
-        process::exit(1);
-    }
-
-    if collected_options.len() >= 20 { // If too many options are passed, there is an issue. 
-        println!("Too many options were passed"); // TODO: Make this number persise.
-        process::exit(1);
-    }
 
     /* if collected_options.len() == 1 { // The help option is the only singular option, therefore it is an error if there is only one collected option and it is not help.
         if collected_options[0] != "--help" && collected_options[0] != "-h" {
@@ -60,9 +49,22 @@ fn main() {
     } */
 }
 
+fn initial_verify_options(collected_arguments_verify: &Vec<String>) {
+    if collected_arguments_verify.len() == 0 { // If no arguments are passed, it is an error.
+        println!("Invalid syntax. Zero arguments were passed. Use \"--help\" or \"-h\" to see options and syntax.");
+        process::exit(1);
+    }
+
+    if collected_arguments_verify.len() > 20 { // If too many arguments are passed, it is an error.
+        println!("Invalid syntax. Too many arguments were passed. Use \"--help\" or \"-h\" to see options and syntax."); // TODO: Make this number more specific to what the actual max is..
+        process::exit(1);
+    }
+}
+
 fn print_help_message() { // Prints the help message and exits.
     println!("Options:");
     println!("-h, --help");
     println!("-p, --path");
+
     process::exit(1);
 }
