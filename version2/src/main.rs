10457e59c4c3121de4350710d63fd67db33372ca
lozlof <lozlofcyber@outlook.com>
2024-11-13 17:22:10 +0000

    Save

--- version2/src/main.rs
+++ version2/src/main.rs
@@ -15,27 +15,30 @@ impl Options {
     fn interpret_and_build_syntax(collected_arguments: Vec<String>) {
         let all_possible_options: [&str; 14] = ["--help", "-h", "--version", "-ver", "--verbose", "-v", "--query", "-q", "--path", "-p", "--simple-grep", "-sg", "--simple-find", "-sf"];
         
-        verify_options_are_valid(&all_possible_options, &collected_arguments);
+        let passed_options: Vec<String> =  verify_options_are_valid(&all_possible_options, &collected_arguments); // Seperates the options (arguments that start with -- or -) from the rest of the arguments. Then compares the options against all_possible_options to see if the given options are valid. If there are bad options, the process ends with an error message. If all the options are valid, it returns the valid options to interpret_and_build_syntax. 
     }
 }
 
-fn verify_options_are_valid(borrow_all_possible_options: &[&str; 14], borrow_collected_arguments: &Vec<String>) {
-    let filtered_options: &Vec<String> = &borrow_collected_arguments // Parses through all the collected arguments and pulls out any options (-- -).
+fn verify_options_are_valid(borrow_all_possible_options: &[&str; 14], borrow_collected_arguments: &Vec<String>) -> Vec<String> {
+    let filtered_options: Vec<String> = borrow_collected_arguments // Parses through all the collected arguments and pulls out any options (-- -).
         .iter() // creates an iterator.
         .filter(|option| option.starts_with("--") || option.starts_with("-")) // .filter(...) is used to retain only items that satisfy a given condition. |option| is a closure (anonymous function) parameter representing each item passed from the iterator. Checks if the String starts with -- or -.
         .cloned() // Is used to convert &String (a reference) into an owned String. This is necessary because we want to create a new vector with owned String values, rather than references to the original vector’s items
         .collect(); //  Takes the filtered and cloned items from the iterator and collects them into a new Vec<String>. This newly created vector is then assigned to filtered_options.
 
-    let bad_options: &Vec<String> = &filtered_options// Parses through all the filtered options and checks if they are actual valid options.
+    let bad_options: Vec<String> = filtered_options// Parses through all the filtered options and checks if they are actual valid options.
         .iter()
-        .filter(|option| !&borrow_all_possible_options.contains(&option.as_str())) // Filters by options that are not contained inside of borrow_all_possible_options.
+        .filter(|option| !borrow_all_possible_options.contains(&option.as_str())) // Filters by options that are not contained inside of borrow_all_possible_options.
         .cloned()
         .collect();
 
     if !bad_options.is_empty() { // If bad_options is not empty, then it means that bad option were passed.
-        let print_bad_options: &String = &bad_options.join(", "); // Turns the values of &bad_options into a string so a clear error message can be printed.
+        let print_bad_options: String = bad_options.join(", "); // Turns the values of &bad_options into a string so a clear error message can be printed.
         println!("Invalid syntax. Unknown options were passed: {}. Use \"--help\" or \"-h\" to see options and syntax.", &print_bad_options);
+        process::exit(1);
     }
+
+    return filtered_options;
 }
 
 fn main() {
